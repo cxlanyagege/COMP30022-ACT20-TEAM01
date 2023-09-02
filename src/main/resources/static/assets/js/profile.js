b56fc49274b6b18fb8b24c344766edc1ffe8b203
cxlanyagege <cxlanyagege@hotmail.com>
2023-09-03 01:32:25 +1000

    Fix an issue where blank page image still displays even when request exists

--- src/main/resources/static/assets/js/profile.js
+++ src/main/resources/static/assets/js/profile.js
@@ -12,25 +12,39 @@ window.onload = async function() {
     // Get tbody from table
     const tableBody = document.getElementById("requestTableBody");
 
-    // Initialize requestId counter
-    let requestId = 1;
+    // Get the blank page image div
+    const blankPageImageDiv = document.querySelector("div[style='display: grid; place-items: center; margin-top: 37px;']");
 
-    // Traverse each requests
-    for (const request of data) {
+    // If no data, show the blank page image
+    if (data.length === 0) {
+        blankPageImageDiv.style.display = "grid";
+    } 
+    
+    // If there is data, hide the image instead
+    else {
+
+        blankPageImageDiv.style.display = "none";
+
+        // Initialize requestId counter
+        let requestId = 1;
+
+        // Traverse each requests
+        for (const request of data) {
         
-        // Create a new td in dashboard
-        const newRow = document.createElement("tr");
-        newRow.innerHTML = `
-            <td>${requestId}</td>
-            <td>${request.subjectCode || ''}</td>
-            <td>${request.description || ''}</td>
-            <td>${request.taskType || ''}</td>
-            <td>${request.status || ''}</td>
-            <td>${request.dueDate || ''}</td>
-        `;
-        requestId++;
+            // Create a new td in dashboard
+            const newRow = document.createElement("tr");
+            newRow.innerHTML = `
+                <td>${requestId}</td>
+                <td>${request.subjectCode || ''}</td>
+                <td>${request.description || ''}</td>
+                <td>${request.taskType || ''}</td>
+                <td>${request.status || ''}</td>
+                <td>${request.dueDate || ''}</td>
+            `;
+            requestId++;
 
-        // Add new rows
-        tableBody.appendChild(newRow);
+            // Add new rows
+            tableBody.appendChild(newRow);
+        }
     }
 };
