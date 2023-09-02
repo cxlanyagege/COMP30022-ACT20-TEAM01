5a53f45e0e3f8ead4bcad2ab0dacd503be309d06
cxlanyagege <cxlanyagege@hotmail.com>
2023-09-03 03:11:17 +1000

    Remove redudancies in login and add jumping back to dashboard after submitting request

--- src/main/resources/static/assets/js/new_request.js
+++ src/main/resources/static/assets/js/new_request.js
@@ -1,31 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const form = document.getElementById('request-form');
+    const form = document.getElementById('request-form');
 
-    form.addEventListener('submit', function(event) {
+        form.addEventListener('submit', function(event) {
 
-      event.preventDefault();
+            event.preventDefault();
 
-      const description = document.getElementById('description').value;
+            const description = document.getElementById('description').value;
 
-      fetch('/api/saveRequest', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + localStorage.getItem('jwt')
-        },
-        body: JSON.stringify({ description: description })
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        if (data.message === 'Request saved successfully!') {
-            alert('Request saved successfully!');
-          }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-    });
-  }
+            fetch('/api/saveRequest', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+                },
+                body: JSON.stringify({ description: description })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.message === 'Request saved successfully!') {
+                    alert('Request saved successfully!');
+                    window.location.href = 'profile.html';
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        });
+    }
 );
