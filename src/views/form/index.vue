27b82c8835fe39843ff805658d9deff20225f688
Lanruos Su <lanruos@student.unimelb.edu.au>
2023-09-17 16:33:08 +1000

    Add Staff Poistion

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -4,6 +4,9 @@
     <el-input v-model="filterText" placeholder="Filter keyword"/>
     <HistoryTable :data="tableData" /> 
     -->
+
+    <!-- Authority Table -->
+
     <el-table 
       v-loading="listLoading"
       :data="tableData" 
@@ -14,12 +17,15 @@
       stripe
       style="width: 100%">
 
+      <el-table-column prop="position" label="Staff Position" ></el-table-column>
       <el-table-column prop="name" label="Staff Name" ></el-table-column>
       <el-table-column prop="email" label="Staff Email" ></el-table-column>
       <el-table-column prop="authority" label="Authority" >
+ 
         <template slot-scope="{ row }">
           <el-switch v-model="row.authority" :active-value="true" :inactive-value="false" active-text="Yes" inactive-text="No"></el-switch>
         </template>
+
       </el-table-column>
     </el-table>
 
@@ -30,20 +36,38 @@
 // import AuthorityTable from ''
 
 export default {
+  
   data() {
     return {
       tableData: [
         {
+          position: 'Coordinator',
           name: 'William Smith',
           email: 'williams@unimelb.edu.au',
           authority: 'Yes'
         },
+
+        {
+          position: 'Lecturer',
+          name: 'David Johnson',
+          email: 'davidj2@unimelb.edu.au',
+          authority: 'Yes'
+        },
         
         {
+          position: 'Head Tutor',
           name: 'James Wilson',
           email: 'jamesw@unimelb.edu.au',
           authority: 'Yes'
+        },
+
+        {
+          position: 'Tutor',
+          name: 'Tom Harris',
+          email: 'tomh6@unimelb.edu.au',        
+          authority: 'Yes'
         }
+
       ],
       listLoading: false
     }
