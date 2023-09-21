15de0d36c122391467579a86e015951f4bb26e02
dennnnisss <wangzeyu@ous-MacBook-Air.local>
2023-09-21 18:55:01 +1000

    i m back again

---
+++ vue/src/api/request.js
@@ -0,0 +1,9 @@
+import { deleteAction, getAction, postAction, putAction } from '@/utils/communication';
+
+export const addRequest = (param) => postAction("/api/saveRequest", param)
+
+export const getRequests = (studentId, param) => getAction("/api/getRequests/" + studentId, param)
+
+export const deleteRequest = (id) => deleteAction("/api/deleteRequest/" + id, null)
+
+export const changeRequest = (id, param) => putAction("/api/changeRequest/" + id, param)
--- vue/src/components/form/index.vue
+++ vue/src/components/form/index.vue
@@ -2,7 +2,7 @@
   <div class="app-container">
     <el-form ref="form" :label-position="labelPosition" :model="form" label-width="120px">
       <el-form-item label="Student ID">
-        <el-input v-model="form.name" />
+        <el-input v-model="form.studentId" />
       </el-form-item>
       <el-form-item label="Request Type">
         <el-select v-model="form.region" placeholder="Please select type">
@@ -30,19 +30,22 @@
         <el-input v-model="form.name" />
       </el-form-item>
       <el-form-item label="Email Alerts">
-        <el-switch v-model="form.delivery" />
+        <el-switch v-model="form.email" />
       </el-form-item>
       <el-form-item label="Request Detail">
-        <el-input v-model="form.desc" type="textarea" />
+        <el-input v-model="form.detail" type="textarea" />
       </el-form-item>
 
       <el-form-item label="Upload File">
         <el-upload
+          v-model:file-list="form.fileList"
           class="upload-demo"
-          action="/your-upload-api-endpoint"
+          action="http://localhost:8080/upload"
           :on-success="handleSuccess"
           :on-error="handleError"
+          :on-remove="handleRemove"
           :accept="'*'"
+          :on-change="handleUploadChange"
         >
           <el-button size="small" type="primary">Click to Upload</el-button>
           <div slot="tip" class="el-upload__tip">You can upload any file format</div>
@@ -58,7 +61,13 @@
 </template>
 
 <script>
+import {getRequests, addRequest, deleteRequest} from '@/api/request'
+import listTable from '@/components/table/index.vue'
+
 export default {
+  components: {
+    listTable
+  },
   data() {
     return {
       labelPosition: 'left',
@@ -72,7 +81,7 @@ export default {
         time: '',
         email: false,
         detail: '',
-        file: null,
+        fileList: [],
         teammates: [] // 存储队友的电子邮件地址
       }
     }
@@ -86,27 +95,48 @@ export default {
   methods: {
     onSubmit() {
       // 创建一个 formData 对象，根据用户选择的动态包含 type 和 name 字段
+      const currentDate = new Date();
+
       const formData = {
         studentId: this.form.studentId,
-        subjectCode: this.form.subjectCode,
-        region: this.form.region,
-        time: this.form.time,
-        email: this.form.email,
-        detail: this.form.detail,
-        file: this.form.file
+        subjectId: this.form.subjectCode,
+        requestType: this.form.region,
+        submissionDate: currentDate,
+        // email: this.form.email,
+        description: this.form.detail,
+        // file: this.form.file
+        requestName: this.form.name
       }
 
       // 如果 showAdditionalOptions 为 true，则包含 type 和 name 字段
       if (this.form.showAdditionalOptions) {
         formData.type = this.form.type
-        formData.name = this.form.name
       }
 
       // 包含队友的电子邮件地址
       formData.teammates = this.form.teammates
 
+      let param = {
+        description: formData.description,
+        studentId: formData.studentId,
+        subjectId: "COMP10012", // 需要从lms上面获取
+        submissionDate: formData.submissionDate,
+        requestType: formData.requestType,
+        requestName: formData.requestName,
+      }
+      addRequest(param).then(res => {
+        console.log(res.data);
+        if (res.data.code == 0){
+          this.$message(res.data.msg)
+          this.$root.$refs.table_component.updateRequests(this.$root.$refs.table_component.pageNum,
+                            this.$root.$refs.table_component.pageSize);
+        } else {
+          this.$message("Fail to submit!")
+        }
+      })
+
       // 现在可以将 formData 发送到服务器或执行其他处理
-      console.log('Form submitted with data:', formData)
+      console.log('Form submitted with data:', param)
 
       this.$message('Submit successful!')
     },
@@ -118,7 +148,17 @@ export default {
     },
     // 处理文件上传成功
     handleSuccess(response, file, fileList) {
-      this.form.file = response.data.fileUrl // 将文件的 URL 存储在表单数据中
+      console.log(response, file, fileList)
+      // this.form.file = response.data.fileUrl // 将文件的 URL 存储在表单数据中
+      // this.form.fileList.push({
+      //   uid: file.data.uid
+      // })
+      //  = fileList.map( item => {
+      //   if (item.uid === res.file.uid){
+      //     item = {uid: item.uid, url: convertUrlWithPrefix(uploadInfo.data)};
+      //   }
+      //   return item;
+      // })
       this.$message.success('File uploaded successfully')
     },
     handleError(err) {
@@ -128,6 +168,9 @@ export default {
       } else {
         this.$message.error('File upload failed')
       }
+    },
+    handleRemove(file, files){
+      console.log(file, files)
     },
     // 添加队友的电子邮件地址输入框
     addTeammate() {
