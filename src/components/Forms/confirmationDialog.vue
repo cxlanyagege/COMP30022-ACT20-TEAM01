f3c1e1efa9dde6d31e965ae62cb19e64833e5ec9
Helenzxx <xuan.zhang9@student.unimelb.edu.au>
2023-10-06 00:21:33 +1100

    drafting modify the approve button dialog

---
+++ src/components/Forms/confirmationDialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-dialog
+    title="Approve Request"
+    :visible.sync="visible"
+    width="30%"
+  >
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="Confirmation Message">
+        <el-input v-model="form.message" placeholder="Enter confirmation message"></el-input>
+      </el-form-item>
+      <el-form-item label="Confirmation Time">
+        <el-date-picker v-model="form.date" type="datetime" placeholder="Select date and time"></el-date-picker>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="handleCancel">Cancel</el-button>
+      <el-button type="primary" @click="handleConfirm">Confirm</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: Boolean,
+    form: Object
+  },
+  methods: {
+    handleCancel() {
+      this.$emit('update:visible', false);
+    },
+    handleConfirm() {
+      this.$emit('confirm');
+    }
+  }
+}
+</script>
