f9ea06c2ee242eaf563af6ce64382861e0b9e3b6
Helenzxx <xuan.zhang9@student.unimelb.edu.au>
2023-10-13 02:21:26 +1100

    change label font on confirmation pop up

--- src/components/Forms/confirmationDialog.vue
+++ src/components/Forms/confirmationDialog.vue
@@ -2,14 +2,14 @@
   <el-dialog
     title="Approve Request"
     :visible.sync="visible"
-    width="30%"
+    width="50%"
   >
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="Confirmation Message">
-        <el-input v-model="form.message" placeholder="Enter confirmation message"></el-input>
+    <el-form ref="form" :model="form" label-width="180px">
+      <el-form-item label="Confirmation Message" class="custom-label-size">
+        <el-input v-model="form.message" placeholder="Enter confirmation message" ></el-input>
       </el-form-item>
-      <el-form-item label="Confirmation Time">
-        <el-date-picker v-model="form.date" type="datetime" placeholder="Select date and time"></el-date-picker>
+      <el-form-item label="Confirmation Time" class="custom-label-size">
+        <el-date-picker v-model="form.date" type="datetime" placeholder="Select date and time" ></el-date-picker>
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
@@ -18,6 +18,12 @@
     </div>
   </el-dialog>
 </template>
+
+<style scoped>
+.custom-label-size .el-form-item__label {
+    font-size: 15px; 
+}
+</style>
 
 <script>
 export default {
